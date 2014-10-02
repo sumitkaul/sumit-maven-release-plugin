8b98f224b29c08cb366f8e42a80d2ca56196d1c7
Sumit Kaul <sumitkaul@fs.fed.us>
2014-10-02 13:52:19 -0400

    test of new release with src changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Sumit Kaul - Test the Maven release plugin results" );
+        System.out.println( "Hello World! Sumit Kaul - Test the Maven release plugin results now" );
     }
 }
