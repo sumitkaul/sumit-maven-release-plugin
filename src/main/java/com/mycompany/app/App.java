5c69f9344e65d2070b8609b4d78a3ac2bdeab14f
Sumit Kaul <sumitkaul@fs.fed.us>
2014-10-02 13:44:14 -0400

    new release from sumit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Sumit Kaul" );
+        System.out.println( "Hello World! Sumit Kaul - Test the Maven release plugin results" );
     }
 }
